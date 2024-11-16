d89c7b78b3b501c9b3c2a1d8df3455fe9d357bb8
AbhishekKolge <abhishekkolge96@gmail.com>
2024-11-16 23:29:28 +0530

    express and app setup added
    Signed-off-by: AbhishekKolge <abhishekkolge96@gmail.com>

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
-console.log("Soar server started");
+require("dotenv").config();
+
+const express = require("express");
+const { shutdown, start } = require("./utils/process");
+
+const app = express();
+
+app.use(express.json());
+
+process.on("SIGINT", shutdown);
+process.on("SIGTERM", shutdown);
+
+start(app);
